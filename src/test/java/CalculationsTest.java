cebf12defa6e55ebcaa60af5be70d95490e72e01
Christian Smit <chrsmit@web.de>
2020-05-17 19:56:17 +0200

    ADD pom.xml and Test Test

--- src/test/java/CalculationsTest.java
+++ src/test/java/CalculationsTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculationsTest {
+    @Test
+    @DisplayName("Test für die POM USW")
+    public void XShouldBeY(){
+        // GIVEN
+
+
+        // WHEN
+
+
+        // THEN
+
+         assertEquals(1,1);
+    }
 
 }
