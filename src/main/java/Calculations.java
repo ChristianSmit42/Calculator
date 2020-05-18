bab5f8957f775589759d8644191ee5178a06b954
Christian Smit <chrsmit@web.de>
2020-05-18 10:28:22 +0200

    ADD

--- src/main/java/Calculations.java
+++ src/main/java/Calculations.java
@@ -1,3 +1,15 @@
 public class Calculations {
 
+    public double summarize(double a, double b) {
+        return a + b;
+    }
+    public double substract(double a, double b){
+        return a-b;
+    }
+    public double multiply(double a, double b){
+        return a*b;
+    }
+    public double divide(double a, double b){
+        return a/b;
+    }
 }
